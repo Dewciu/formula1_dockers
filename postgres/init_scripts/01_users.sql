c8772de7a812c2cc14be83d94c4d154075e9c04e
Dewciu <kacperkrol@protonmail.com>
2024-01-13 21:37:46 +0100

    init scripts modify

--- postgres/init_scripts/01_users.sql
+++ postgres/init_scripts/01_users.sql
@@ -6,7 +6,3 @@ CREATE TABLE IF NOT EXISTS users (
   is_admin BOOLEAN DEFAULT false,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO public.users (username,password_hash,email,created_at,is_admin) VALUES
-	 ('Dew','$2b$12$EoWjOlFvN8PUxXnqryyG8..Z1QKKy86YWf7J7.UwtZNizBUgmpUr2','kacperkrol@protonmail.com','2024-01-06 18:12:28.493934',true),
-	 ('JakubStarosta','$2b$12$i96AjfC28Nxp8m6wFSqNAecEAvNlYgpVHEbyGAMLz8T18PY4pECAi','chujwieco@gmail.com','2024-01-06 19:01:59.113004',false);
--- postgres/init_scripts/02_charts.sql
+++ postgres/init_scripts/02_charts.sql
@@ -1,15 +1,12 @@
 CREATE TABLE IF NOT EXISTS charts (
   id serial PRIMARY KEY,
+  identifier VARCHAR (50) UNIQUE NOT NULL,
   title VARCHAR (50) UNIQUE NOT NULL,
   filename VARCHAR (50) UNIQUE NOT NULL,
   description VARCHAR (10000) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO public.charts (title,filename,description,created_at) VALUES
-	 ('Ferrari SF23','img1.png','Test1desct','2024-01-06 19:13:14.025891'),
-	 ('Max and Lewis','img2.jpg','Max Verstappen i Lewis Hamilton','2024-01-06 19:14:18.039969'),
-	 ('F1 Logo','img3.png','Logo of Formula 1','2024-01-06 19:14:18.042431'),
-	 ('Charles Leclerc','img4.jpg','Charles Leclerc, właśc. Charles Marc Hervé Percival Leclerc[1] (ur. 16 października 1997 w Monako) – monakijski kierowca wyścigowy, startujący w mistrzostwach świata Formuły 1 od sezonu 2018. Od sezonu 2019 kierowca zespołu Scuderia Ferrari, zwycięzca pięciu wyścigów Formuły 1. Wicemistrz świata Formuły 1 (2022), mistrz Formuły 2 (2017), mistrz serii GP3 (2016).
-
-W latach 2016–2017 członek Akademii Kierowców Ferrari, uznany przez Międzynarodową Federację Samochodową (FIA) za najlepszego debiutanta sezonu dwa razy z rzędu w latach 2017–2018, zarówno w Formule 2, jak i w Formule 1. W wieku 21 lat został kierowcą zespołu Scuderia Ferrari, przez co został najmłodszym kierowcą zakontraktowanym przez ten zespół od 1961 roku.','2024-01-06 19:14:18.043972');
+INSERT INTO public.charts (identifier,title,filename,description,created_at) VALUES
+	 ('most_successful_drivers','Most successful drivers per season','img1.jpg','A chart showing the number of points obtained by a given driver in a given season and its trend throughout the season. To generate a chart, please enter the season. With optional driver name entry, the chart generates only the desired results.','2024-01-06 19:13:14.025891'),
+	 ('most_wins_drivers','Drivers with most wins','lewis_schumi.jpeg','A graph showing the "n" most winning drivers. Please enter how many top drivers should be generated.','2024-01-06 19:14:18.039969');
